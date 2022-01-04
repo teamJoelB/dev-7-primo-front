d399bb59e52c8123fbc92924421b164506559bdd
lacyohann <yohann.lac.perso@gmail.com>
2022-01-04 16:50:17 +0100

    l

---
+++ calcul.js
@@ -0,0 +1,19 @@
+/*
+function calcul(){
+    a = 0;
+    b = 0;
+    max = 10;
+    a = Math.floor(Math.random() * max);
+    b = Math.floor(Math.random() * max);
+}
+*/
+
+function resultat(a,b){
+
+    var reponse = document.querySelector('#nb').value;
+
+    if(reponse == a + b)
+        score ++;
+    else
+        score --;
+}
